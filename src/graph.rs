27f6d34b780b5e5d713e617676fe364b0055b69c
Amos Jun-yeung Ng <me@amos.ng>
2020-09-21 09:22:17 -0700

    Create in-memory graph implementation

--- src/graph.rs
+++ src/graph.rs
@@ -1,7 +1,47 @@
 //! Graph implementations
 
+use petgraph::graph::{NodeIndex};
+
 /// A classic directed Graph with nodes and labeled links.
-pub trait Graph {
+pub trait Graph<'a> {
     /// Adds a new node with the given string name to the graph, and returns the node's ID.
-    fn add_node(self, name: String) -> u32;
+    fn add_node(&mut self, name: &'a str) -> usize;
+}
+
+pub fn new_in_memory_graph<'a>() -> impl Graph<'a> {
+    InMemoryGraph::new()
+}
+
+struct InMemoryGraph<'a> {
+    graph: petgraph::graph::Graph::<&'a str, NodeIndex>
+}
+
+impl<'a> InMemoryGraph<'a> {
+    fn new() -> Self {
+        InMemoryGraph {
+            graph: petgraph::graph::Graph::<&'a str, NodeIndex>::new()
+        }
+    }
+}
+
+impl<'a> Graph<'a> for InMemoryGraph<'a> {
+    fn add_node(&mut self, name: &'a str) -> usize {
+        return self.graph.add_node(name).index();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn creates_in_memory_graph() {
+        new_in_memory_graph();
+    }
+
+    #[test]
+    fn add_node_to_in_memory_graph() {
+        let mut g = new_in_memory_graph();
+        g.add_node("A");
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,3 @@
 //! Yin is a rudimentary, experimental knowledge base.
 
 pub mod graph;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
