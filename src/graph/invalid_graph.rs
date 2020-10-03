b57ddfd2f9c4d8218541f04013734124c33b4211
Amos Jun-yeung Ng <me@amos.ng>
2020-10-03 00:30:54 -0700

    Reuse same invalid message in InvalidGraph

--- src/graph/invalid_graph.rs
+++ src/graph/invalid_graph.rs
@@ -5,52 +5,56 @@ use std::rc::Rc;
 /// Invalid default graph.
 pub struct InvalidGraph {}
 
+impl InvalidGraph {
+    const INVALID_MSG: &'static str = "Initialize graph binding before use";
+}
+
 impl Graph for InvalidGraph {
     fn add_node(&mut self) -> usize {
-        panic!("Initialize graph binding before use");
+        panic!(Self::INVALID_MSG);
     }
 
     fn set_node_value(&mut self, _: usize, _: Box<dyn KBWrapper>) {
-        panic!("Initialize graph binding before use");
+        panic!(Self::INVALID_MSG);
     }
 
     fn set_node_name(&mut self, _: usize, _: String) {
-        panic!("Initialize graph binding before use");
+        panic!(Self::INVALID_MSG);
     }
 
     fn node_name(&self, _: usize) -> Option<Rc<String>> {
-        panic!("Initialize graph binding before use");
+        panic!(Self::INVALID_MSG);
     }
 
     fn node_value(&self, _: usize) -> Option<Rc<Box<dyn KBWrapper>>> {
-        panic!("Initialize graph binding before use");
+        panic!(Self::INVALID_MSG);
     }
 
     fn add_edge(&mut self, _: usize, _: usize, _: usize) {
-        panic!("Initialize graph binding before use");
+        panic!(Self::INVALID_MSG);
     }
 
     fn has_edge(&self, _: usize, _: usize, _: usize) -> bool {
-        panic!("Initialize graph binding before use");
+        panic!(Self::INVALID_MSG);
     }
 
     fn outgoing_nodes(&self, _: usize, _: usize) -> Vec<usize> {
-        panic!("Initialize graph binding before use")
+        panic!(Self::INVALID_MSG)
     }
 
     fn incoming_nodes(&self, _: usize, _: usize) -> Vec<usize> {
-        panic!("Initialize graph binding before use")
+        panic!(Self::INVALID_MSG)
     }
 
     fn all_outgoing_nodes(&self, _: usize) -> Vec<usize> {
-        panic!("Initialize graph binding before use")
+        panic!(Self::INVALID_MSG)
     }
 
     fn all_incoming_nodes(&self, _: usize) -> Vec<usize> {
-        panic!("Initialize graph binding before use")
+        panic!(Self::INVALID_MSG)
     }
 
     fn into_dot(&self) -> String {
-        panic!("Initialize graph binding before use")
+        panic!(Self::INVALID_MSG)
     }
 }
