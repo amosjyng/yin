60db4b3bc486b68fcd5659f8b37173a0f5b5ac4c
Amos Jun-yeung Ng <me@amos.ng>
2020-10-08 22:49:35 -0700

    Move hello-world example to rustdoc for more visible, but still testable, documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,25 @@
 //! Yin is a rudimentary, experimental knowledge base.
+//!
+//! # Example
+//!
+//! ```rust
+//! use zamm_yin::concepts::{ArchetypeTrait, FormTrait, Tao};
+//! use zamm_yin::graph::bind_in_memory_graph;
+//! use zamm_yin::wrappers::CommonNodeTrait;
+//!
+//! fn main() {
+//!     // Initialize the knowledge-base
+//!     bind_in_memory_graph();
+//!
+//!     // Create a new concept
+//!     let mut concept = Tao::individuate();
+//!     assert!(concept.has_ancestor(Tao::archetype()));
+//!
+//!     // Set a name for the concept
+//!     concept.set_internal_name("Hello, world.".to_string());
+//!     println!("{}", concept.internal_name().unwrap());
+//! }
+//! ```
 
 #![warn(missing_docs)]
 
