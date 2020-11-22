755eea43f7620c9fedfc2f3771bf6d501b3c2b6c
Amos Jun-yeung Ng <me@amos.ng>
2020-11-21 19:09:33 -0800

    Revert "Creating release v0.1.1"
    This reverts commit 53029683ff733e42eba70b9706dbf330897feb39.

---
+++ build.rs
@@ -0,0 +1,5 @@
+use zamm::generate_yin_code;
+
+fn main() {
+    generate_yin_code("yin.md").unwrap();
+}
--- src/tao/form/mod.rs
+++
@@ -1,9 +0,0 @@
-//! Concept forms, as opposed to archetypes.
-
-pub mod data;
-
-mod form_form;
-mod form_trait;
-
-pub use form_form::Form;
-pub use form_trait::FormTrait;
