53029683ff733e42eba70b9706dbf330897feb39
Amos Jun-yeung Ng <me@amos.ng>
2020-11-21 19:02:11 -0800

    Creating release v0.1.1

--- build.rs
+++
@@ -1,5 +0,0 @@
-use zamm::generate_yin_code;
-
-fn main() {
-    generate_yin_code("yin.md").unwrap();
-}
---
+++ src/tao/relation/flag/mod.rs
@@ -0,0 +1,5 @@
+//! Relations involving only one form.
+
+mod flag_form;
+
+pub use flag_form::Flag;
